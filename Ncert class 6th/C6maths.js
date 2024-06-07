73a5ccab040548d851463b65a685840c62cf4112
Vivek Chahar <78673919+VivekChahar1960@users.noreply.github.com>
2024-06-07 12:44:28 +0530

    Add files via upload

---
+++ Ncert class 6th/C6maths.js
@@ -0,0 +1,59 @@
+//dropdown  clickable 
+
+
+function showDrpDwn(){
+    var click = document.getElementById("dropdown-options");
+        if (click.style.display === "none") {
+           click.style.display = "block";
+        } else {
+           click.style.display = "none";
+        }
+  }
+  function showDrpDwn2(){
+    var click = document.getElementById("dropdown-options2");
+        if (click.style.display === "none") {
+           click.style.display = "block";
+        } else {
+           click.style.display = "none";
+        }
+  }
+  function showDrpDwn3(){
+    var click = document.getElementById("dropdown-options3");
+        if (click.style.display === "none") {
+           click.style.display = "block";
+        } else {
+           click.style.display = "none";
+        }
+  }
+  function showDrpDwn4(){
+    var click = document.getElementById("dropdown-options4");
+        if (click.style.display === "none") {
+           click.style.display = "block";
+        } else {
+           click.style.display = "none";
+        }
+  }
+  function showDrpDwn5(){
+    var click = document.getElementById("dropdown-options5");
+        if (click.style.display === "none") {
+           click.style.display = "block";
+        } else {
+           click.style.display = "none";
+        }
+  }
+  
+  // menu btn click functuion
+  function clickfunctionMenu(){
+    var x=document.getElementById("menu");
+    if (x.className === "menu") {
+      x.className += " responsive";
+      
+    } else {
+      x.className = "menu";
+    }
+  }
+
+
+
+
+
