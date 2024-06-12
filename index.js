2f015164f8b7aa383045b8fdbe391bfc7d733bc1
Vivek Chahar <78673919+VivekChahar1960@users.noreply.github.com>
2024-06-12 12:57:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -104,6 +104,308 @@ function clickfunction(){
 }
 
 
+window.addEventListener('scroll', function(){
+  const x=document.querySelector('#links');
+  if(document.documentElement.scrollTop > 190){
+    x.classList.add("stickynavbar");
+  }else{
+    x.classList.remove("stickynavbar");
+  }
+});
+
+
+function feedbackclicked(){
+  var x=document.getElementById("feebackform");
+  var y=document.getElementById("stickyFeedback")
+  var z=document.getElementById("header");
+  var w=document.getElementById("slideshow-container");
+  var u=document.getElementById("chooseUs");
+  var t=document.getElementById("Aboutus");
+  var s=document.getElementById("Aboutweb");
+  var r=document.getElementById("Query");
+  var p=document.getElementById("container");
+  var q=document.getElementById("footer");
+
+  if (x.className === "feebackform") {
+    x.style.top="30%";
+    y.style.visibility="hidden";
+    z.style.filter="blur(4px)";
+    w.style.filter="blur(4px)";
+    u.style.filter="blur(4px)";
+    t.style.filter="blur(4px)";
+    s.style.filter="blur(4px)";
+    r.style.filter="blur(4px)";
+    p.style.filter="blur(4px)";
+    q.style.filter="blur(4px)";
+
+  } else {
+    x.className = "feebackform";
+  }
+}
+
+function emojiclicked1(){
+  var s=document.getElementById("emojiangry");
+  var y=document.getElementById("howrate");
+  var z=document.getElementById("rateusclicked");
+  var a=document.getElementById("emojinormal");
+  var b=document.getElementById("emojihappy");
+  var c=document.getElementById("emojiveryhappy");
+  var d=document.getElementById("emojisad");
+  s.style.opacity="1";
+  y.style.display="none";
+  z.style.display="flex";
+  a.style.display="none";
+  b.style.display="none";
+  c.style.display="none";
+  d.style.display="none";
+}
+function emojiclicked2(){
+  var s=document.getElementById("emojisad");
+  var y=document.getElementById("howrate");
+  var z=document.getElementById("rateusclicked");
+  var a=document.getElementById("emojinormal");
+  var b=document.getElementById("emojihappy");
+  var c=document.getElementById("emojiveryhappy");
+  var d=document.getElementById("emojiangry");
+  s.style.opacity="1";
+  y.style.display="none";
+  z.style.display="flex";
+  a.style.display="none";
+  b.style.display="none";
+  c.style.display="none";
+  d.style.display="none";
+}
+function emojiclicked3(){
+  var s=document.getElementById("emojinormal");
+  var y=document.getElementById("howrate");
+  var z=document.getElementById("rateusclicked");
+  var a=document.getElementById("emojiangry");
+  var b=document.getElementById("emojihappy");
+  var c=document.getElementById("emojiveryhappy");
+  var d=document.getElementById("emojisad");
+  s.style.opacity="1";
+  y.style.display="none";
+  z.style.display="flex";
+  a.style.display="none";
+  b.style.display="none";
+  c.style.display="none";
+  d.style.display="none";
+}
+function emojiclicked4(){
+  var s=document.getElementById("emojihappy");
+  var y=document.getElementById("howrate");
+  var z=document.getElementById("rateusclicked");
+  var a=document.getElementById("emojinormal");
+  var b=document.getElementById("emojiangry");
+  var c=document.getElementById("emojiveryhappy");
+  var d=document.getElementById("emojisad");
+  s.style.opacity="1";
+  y.style.display="none";
+  z.style.display="flex";
+  a.style.display="none";
+  b.style.display="none";
+  c.style.display="none";
+  d.style.display="none";
+}
+function emojiclicked5(){
+  var s=document.getElementById("emojiveryhappy");
+  var y=document.getElementById("howrate");
+  var z=document.getElementById("rateusclicked");
+  var a=document.getElementById("emojinormal");
+  var b=document.getElementById("emojihappy");
+  var c=document.getElementById("emojiangry");
+  var d=document.getElementById("emojisad");
+  s.style.opacity="1";
+  y.style.display="none";
+  z.style.display="flex";
+  a.style.display="none";
+  b.style.display="none";
+  c.style.display="none";
+  d.style.display="none";
+}
+
+function feedbackclose(){
+  var a=document.getElementById("emojiangry");
+  var b=document.getElementById("emojisad");
+  var c=document.getElementById("emojinormal");
+  var d=document.getElementById("emojihappy");
+  var e=document.getElementById("emojiveryhappy");
+  var y=document.getElementById("feebackform");
+  var x=document.getElementById("stickyFeedback")
+  var z=document.getElementById("header");
+  var w=document.getElementById("slideshow-container");
+  var u=document.getElementById("chooseUs");
+  var t=document.getElementById("Aboutus");
+  var s=document.getElementById("Aboutweb");
+  var r=document.getElementById("Query");
+  var p=document.getElementById("container");
+  var q=document.getElementById("footer");
+  var j=document.getElementById("howrate");
+  var k=document.getElementById("rateusclicked");
+  var l=document.getElementById("emojitext");
+  y.style.top="-70%";
+  x.style.visibility="visible";
+  z.style.filter="none";
+  w.style.filter="none";
+  u.style.filter="none";
+  t.style.filter="none";
+  s.style.filter="none";
+  r.style.filter="none";
+  p.style.filter="none";
+  q.style.filter="none";
+  a.style.display="flex";
+  a.style.opacity="0.5";
+  a.addEventListener('mouseover' ,()=>{
+    a.style.opacity="1";
+  })
+  a.addEventListener('mouseout',()=>{
+    a.style.opacity="0.5";
+  })
+
+
+  b.style.display="flex";
+  b.style.opacity="0.5";
+  b.addEventListener('mouseover' ,()=>{
+    b.style.opacity="1";
+  })
+  b.addEventListener('mouseout',()=>{
+    b.style.opacity="0.5";
+  })
+
+
+  c.style.display="flex";
+  c.style.opacity="0.5";
+  c.addEventListener('mouseover' ,()=>{
+    c.style.opacity="1";
+  })
+  c.addEventListener('mouseout',()=>{
+    c.style.opacity="0.5";
+  })
+
+  d.style.display="flex";
+  d.style.opacity="0.5";
+  d.addEventListener('mouseover' ,()=>{
+    d.style.opacity="1";
+  })
+  d.addEventListener('mouseout',()=>{
+    d.style.opacity="0.5";
+  })
+
+  e.style.display="flex";
+  e.style.opacity="0.5";
+  e.addEventListener('mouseover' ,()=>{
+    e.style.opacity="1";
+  })
+  e.addEventListener('mouseout',()=>{
+    e.style.opacity="0.5";
+  })
+
+  y.style.top="-70%";
+  x.style.visibility="visible";
+  z.style.filter="none";
+  w.style.filter="none";
+  u.style.filter="none";
+  t.style.filter="none";
+  s.style.filter="none";
+  r.style.filter="none";
+  p.style.filter="none";
+  q.style.filter="none";
+  j.style.display="flex";
+  k.style.display="none";
+  l.style.display="flex";
+}
+
+function submitbtn(){
+  var a=document.getElementById("emojiangry");
+  var b=document.getElementById("emojisad");
+  var c=document.getElementById("emojinormal");
+  var d=document.getElementById("emojihappy");
+  var e=document.getElementById("emojiveryhappy");
+  var y=document.getElementById("feebackform");
+  var x=document.getElementById("stickyFeedback")
+  var z=document.getElementById("header");
+  var w=document.getElementById("slideshow-container");
+  var u=document.getElementById("chooseUs");
+  var t=document.getElementById("Aboutus");
+  var s=document.getElementById("Aboutweb");
+  var r=document.getElementById("Query");
+  var p=document.getElementById("container");
+  var q=document.getElementById("footer");
+  var j=document.getElementById("howrate");
+  var k=document.getElementById("rateusclicked");
+  var l=document.getElementById("emojitext");
+  y.style.top="-70%";
+  x.style.visibility="visible";
+  z.style.filter="none";
+  w.style.filter="none";
+  u.style.filter="none";
+  t.style.filter="none";
+  s.style.filter="none";
+  r.style.filter="none";
+  p.style.filter="none";
+  q.style.filter="none";
+  a.style.display="flex";
+  a.style.opacity="0.5";
+  a.addEventListener('mouseover' ,()=>{
+    a.style.opacity="1";
+  })
+  a.addEventListener('mouseout',()=>{
+    a.style.opacity="0.5";
+  })
+
+
+  b.style.display="flex";
+  b.style.opacity="0.5";
+  b.addEventListener('mouseover' ,()=>{
+    b.style.opacity="1";
+  })
+  b.addEventListener('mouseout',()=>{
+    b.style.opacity="0.5";
+  })
+
+
+  c.style.display="flex";
+  c.style.opacity="0.5";
+  c.addEventListener('mouseover' ,()=>{
+    c.style.opacity="1";
+  })
+  c.addEventListener('mouseout',()=>{
+    c.style.opacity="0.5";
+  })
+
+  d.style.display="flex";
+  d.style.opacity="0.5";
+  d.addEventListener('mouseover' ,()=>{
+    d.style.opacity="1";
+  })
+  d.addEventListener('mouseout',()=>{
+    d.style.opacity="0.5";
+  })
+
+  e.style.display="flex";
+  e.style.opacity="0.5";
+  e.addEventListener('mouseover' ,()=>{
+    e.style.opacity="1";
+  })
+  e.addEventListener('mouseout',()=>{
+    e.style.opacity="0.5";
+  })
+
+  y.style.top="-70%";
+  x.style.visibility="visible";
+  z.style.filter="none";
+  w.style.filter="none";
+  u.style.filter="none";
+  t.style.filter="none";
+  s.style.filter="none";
+  r.style.filter="none";
+  p.style.filter="none";
+  q.style.filter="none";
+  j.style.display="flex";
+  k.style.display="none";
+  l.style.display="flex";
+}
+
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
